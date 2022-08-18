63b5232e9a3022586a084046cff95d745f852f86
Alexander Olofsson <ace@haxalot.com>
2022-08-18 23:37:08 +0200

    Reworking communication checking

--- SERESTPlugin/Util/MyCubeGridExtensions.cs
+++ SERESTPlugin/Util/MyCubeGridExtensions.cs
@@ -1,25 +1,15 @@
+using System.Linq;
 using Sandbox.Game.Entities;
-using Sandbox.ModAPI;
-using Sandbox.ModAPI.Interfaces;
-using SpaceEngineers.Game.ModAPI;
+using Sandbox.Game.World;
 
 namespace SERESTPlugin.Util
 {
 
 static class MyCubeGridExtensions
 {
-    public static bool PlayerCanCommunicate(this MyCubeGrid grid, Player player)
+    public static bool PlayerCanCommunicate(this MyCubeGrid grid, MyPlayer player)
     {
-        var distance = (player.Character.PositionComp.GetPosition() - grid.PositionComp.GetPosition()).Length();
-        if (grid.GridSystems.RadioSystem.Receivers.Any(r => r.CanBeUsedByPlayer(player.ID)))
-        {
-            var antennas = grid.GetFatBlocks().OfType<IMyRadioAntenna>().Where(b => b.IsWorking);
-            if (antennas.Any(ant => distance <= ant.Radius))
-                return true;
-        }
-
-        if (distance < 1000)
-            return true;
+        var receivers = grid.GridSystems.RadioSystem.Receivers.Where(r => r.CanBeUsedByPlayer(player.Identity.IdentityId));
 
         return false;
     }
